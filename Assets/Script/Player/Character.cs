bfd39c123a596c34315338723f163032f00788e0
donggov <donggov@gmail.com>
2019-09-15 21:01:37 +0900

    플레이어 이동

---
+++ Assets/Script/Player/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+    protected Vector2 direction;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+        AnimateMovement();
+    }
+
+    public void Move()
+    {
+        if (direction.x > 0) spriteRenderer.flipX = false;
+        else if (direction.x < 0) spriteRenderer.flipX = true;
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void AnimateMovement()
+    {
+        animator.SetBool("isWalk", (direction.x > 0 || direction.x < 0 || direction.y > 0 || direction.y < 0));
+    }
+
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Character
+{
+    void Start()
+    {
+    }
+
+    protected override void Update()
+    {   
+        GetInput();
+        base.Update();
+    }
+
+    private void GetInput()
+    {
+        Vector2 moveVector;
+
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+
+        direction = moveVector;
+    }
+
+}
